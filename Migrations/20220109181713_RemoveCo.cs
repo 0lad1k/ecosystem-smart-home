55dcdeb262bfe2a611ab73773426a92c0d299231
Artur Dzhus <asasin.lol10@gmail.com>
2022-01-09 20:19:13 +0200

    -Add migration

---
+++ Migrations/20220109181713_RemoveCo.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EcosystemSmartHome.Migrations
+{
+    public partial class RemoveCo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "quantity_co",
+                table: "room_info");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<double>(
+                name: "quantity_co",
+                table: "room_info",
+                type: "double precision",
+                nullable: false,
+                defaultValue: 0.0);
+        }
+    }
+}
